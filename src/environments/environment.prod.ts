dbe9291d07c912d19062d10caf0816cb97dc58e1
DawidPlociennikDev <37553365+DawidPlociennikDev@users.noreply.github.com>
2020-11-11 20:23:58 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,11 +3,11 @@ export const environment = {
 };
 
 export const firebaseConfig = {
-  apiKey: 'AIzaSyANka3bhfzVI2kNARFvcuyTfxaWQRQJGLo',
-  authDomain: 'browarapp.firebaseapp.com',
-  databaseURL: 'https://browarapp.firebaseio.com',
-  projectId: 'browarapp',
-  storageBucket: 'browarapp.appspot.com',
-  messagingSenderId: '883017495908',
-  appId: '1:883017495908:web:2c4fa4e6f4558ebae5b671'
+  apiKey: '###',
+  authDomain: '###',
+  databaseURL: '###',
+  projectId: '###',
+  storageBucket: '###',
+  messagingSenderId: '###',
+  appId: '###'
 }
